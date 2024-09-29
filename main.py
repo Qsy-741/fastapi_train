534cc0c6184fd3f2141ece05479b1f7a34a2b112
qsy-f <qsy_f@123.com>
2024-09-29 18:34:17 +0800

    整体更新

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # 不是这怎么这么难用啊
+from typing import Union
 
-print("nihao ")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
