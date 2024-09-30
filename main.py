b85405f384161e6a724a75e034d57fff590f0101
qsy-f <qsy_f@123.com>
2024-09-30 11:25:06 +0800

    整体更新

--- main.py
+++ main.py
@@ -22,3 +22,8 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
