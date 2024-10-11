56a2b1b81627b96733eef8d872d3ca8cb31a9985
qsy-f <qsy_f@123.com>
2024-10-11 14:08:45 +0800

    post练习13:04

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
 # 不是这怎么这么难用啊
+# taskkill /PID 17308 /F
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
+# 查询参数类型转换
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# 可选参数
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# 查询参数
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip + limit]
+
+# 路径参数
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/users")
+async def read_user(user_id: str):
+    return {"user_id"}
+
+
+@app.get("/users")
+async def read_user_me():
+    return ["user_id", "the current user"]
 
